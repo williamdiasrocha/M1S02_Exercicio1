a7fe3e3153d374334a11ea78d487a8420c981d6a
William Rocha <uill_pf@hotmail.com>
2023-02-10 21:49:33 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+/*       ***RESPOSTAS***
+
+     1 - Qual a descrição de um paradigma de POO ?
+
+ "Um paradigma pode ser entendido como a forma com a qual se decide resolver determinado problema por meio da programação de computadores. 
+Na POO um paradigma é um modelo de análise, projeto e programação baseado na aproximação entre o mundo real e o mundo virtual, através da criação e interação entre objetos, atributos, códigos, métodos, entre outros."
+
+
+
+
+
+     2 - Quais os quatro pilares de POO ?
+
+ Os 4 pilares de POO são: Polimorfismo, Herança, Abstração e Encapsulamento.  
+ 
+ 
+
+
+
+     3 - Cite uma  estrutura de classe conforme a aula 1 ?
+ 
+ public class Empresa
+ {
+
+    private int id;
+   
+    public int Nome { get; set; }
+
+
+
+
+    public Empresa(int id, string nome)
+    {
+        this.id = id;
+        this.Nome = nome;
+    }
+
+    public string RetornarDadosFuncao() => $"Registro {id} - Nome {Nome}";
+
+    public void AlterarNome(string nomeAlterar)
+    {
+        Nome = nomeAlterar;
+    }
+ }
+ 
+ */
